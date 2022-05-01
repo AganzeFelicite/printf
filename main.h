b77f232bc9ee7cc9a79bec8c792e984661d70adc
AganzeFelicite <aganzefelicite@gmail.com>
2022-05-01 15:58:47 -0700

    the header file

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
